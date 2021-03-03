7bcef1d623fd8788c4e97108897e1c093fa168dc
jganchozo <jganchozo@outlook.com>
2021-03-03 13:21:29 -0500

    Form Components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Header  from "./components/Header";
+import Formulario  from "./components/Formulario";
 import styled from "@emotion/styled";
 
 const Contenedor = styled.div`
@@ -16,7 +17,7 @@ function App() {
     <Contenedor>
       <Header titulo="Cotizador de Seguros"/>
       <ContenedorFormulario>
-        
+        <Formulario />
       </ContenedorFormulario>
     </Contenedor>
   );
